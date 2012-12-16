e082db05b8c5f4eeea01a01e136a5d34b60ca97e
Peter Wortmann <scpmw@leeds.ac.uk>
2012-12-16 13:37:00 +0000

    Sample maps
    The goal here is to quickly get an overview on where in the profile
    certain IP ranges appear. We optimize mainly two aspects - first, we
    build a map with all samples of a certain type that can be found in the
    profile (this will probably be handy for other things too). Second, we
    make extra sure that the sampling loop doesn't do double-checks or ends
    up using IP ranges where higher-priority ones exist. The result gets
    converted into a time-map for fast overview generation.

--- Events/Debug.hs
+++ Events/Debug.hs
@@ -10,29 +10,37 @@ module Events.Debug (
 
   lookupRange, lookupRanges,
 
-  lookupCore, findAllocationEntry
+  lookupCore, findAllocationEntry,
+
+  getSumTimeTree, getSampleTimeTree, SampleWeight(..)
 
   ) where
 
 import Prelude
 
 import Events.Core as Core
+import Events.TimeTree
 
 import GHC.RTS.Events
 
 import Data.Array
 import Data.Char (chr, ord)
-import Data.Maybe (mapMaybe, isJust)
+import Data.Maybe (mapMaybe, isJust, fromMaybe)
+import Data.Monoid (Monoid(..))
 import Data.List
+import Data.Ord  (comparing)
 import Data.Word (Word64)
 import qualified Data.Function as F
 import qualified Data.IntMap as IM
 import qualified Data.Map as Map
 import qualified Data.ByteString as BS
+import qualified Data.Array.Unboxed as UA
 
 import Text.Printf
 
 import Debug.Trace
+
+type CapNo = Int
 
 type RangeMap = IM.IntMap DebugEntry
 type DbgMap = [DebugEntry]
@@ -49,8 +57,8 @@ data Span = Span {
   } deriving (Eq, Ord)
 
 data IPRange = IPRange {
-  _rangeStart :: {-# UNPACK #-} !Int,
-  _rangeEnd   :: {-# UNPACK #-} !Int
+  rangeStart :: {-# UNPACK #-} !Int,
+  rangeEnd   :: {-# UNPACK #-} !Int
   }
 instance Show IPRange where
   show (IPRange x y) = printf "%06x-%06x" x y
@@ -79,16 +87,31 @@ instance Eq DebugEntry where
 instance Ord DebugEntry where
   compare = compare `F.on` dbgId
 
+newtype SampleWeight = SampleWeight { unSampleWeight :: Word64 } deriving (Show)
+
+instance Monoid SampleWeight where
+  mempty                          = SampleWeight 0
+  (SampleWeight a) `mappend` (SampleWeight b) = SampleWeight $ a + b
+
+data SampleMap = SampleMap {
+  smapCap :: CapNo,
+  smapVerb :: SampleVerb,
+  smapNoun :: SampleNoun,
+  smapSamples :: IM.IntMap Event, -- ^ All matching samples, by time
+  smapSampleWeight :: TimeTree SampleWeight   -- ^ Sum of weights over a given time period
+  }
 
 data DebugMaps = DebugMaps {
-  rangeMap :: RangeMap,
-  coreMap :: CoreMap,
-  _sourceMap :: SourceMap
+  rangeMap   :: RangeMap,
+  coreMap    :: CoreMap,
+  _sourceMap :: SourceMap,
+  sampleMaps :: [SampleMap],
+  endTime    :: Timestamp
   }
 
 
 emptyDebugMaps :: DebugMaps
-emptyDebugMaps = DebugMaps IM.empty Map.empty Map.empty
+emptyDebugMaps = DebugMaps IM.empty Map.empty Map.empty [] 0
 
 buildDebugMaps :: Array Int CapEvent -> (String -> IO ()) -> IO (DebugMaps, Array Int CapEvent)
 buildDebugMaps eventsArr progress = do
@@ -111,9 +134,18 @@ buildDebugMaps eventsArr progress = do
 
   -- Build source map
   let sourceMap = buildSourceMap dbgMap
-  putStrLn $ printf "Source map has %d entries" (Map.size sourceMap)
+  --putStrLn $ printf "Source map has %d entries" (Map.size sourceMap)
 
-  return (DebugMaps rangeMap coreMap sourceMap, eventsArr)
+  -- Build sample map
+  let lastIx = snd $ bounds eventsArr
+      endTime | lastIx > snd (bounds eventsArr)              = 0
+              | CapEvent _ (Event t _) <- eventsArr ! lastIx = t
+  let sampleMaps = buildSampleMaps eventsArr startIx endTime
+      sampleCount = IM.size . smapSamples
+  putStrLn $ printf "Sample map has %d types, %d entries total"
+    (length sampleMaps) (sum $ map sampleCount sampleMaps)
+
+  return (DebugMaps rangeMap coreMap sourceMap sampleMaps endTime, eventsArr)
 
 -------------------------------------------------------------------------------
 
@@ -335,6 +367,122 @@ buildSourceMap :: [DebugEntry] -> SourceMap
 buildSourceMap = Map.fromListWith (++) . concatMap mkEntry
   where mkEntry entry@DebugEntry{dbgSources} =
           map (\s -> (s, [entry])) dbgSources
+
+-------------------------------------------------------------------------------
+
+lookupSampleMap :: [SampleMap] -> CapNo -> SampleVerb -> SampleNoun -> Maybe SampleMap
+lookupSampleMap []                     _    _     _
+  = Nothing
+lookupSampleMap (m@SampleMap{..}:maps) cap verb noun
+  | cap == smapCap && verb == smapVerb && noun == smapNoun
+  = Just m
+  | otherwise
+  = lookupSampleMap maps cap verb noun
+
+buildSampleMaps :: Array Int CapEvent -> Int -> Timestamp -> [SampleMap]
+buildSampleMaps arr n endTime = map setSampleWeights $ go n []
+  where
+    (_, end) = bounds arr
+    go n maps
+      | n > end   = maps
+      | CapEvent _ ev@(Event t (Samples {cap, sample_by, sample_type})) <- arr ! n
+                  = let update []
+                          = [SampleMap { smapCap = cap
+                                       , smapVerb = sample_by
+                                       , smapNoun = sample_type
+                                       , smapSamples = IM.singleton (fromIntegral t) ev 
+                                       , smapSampleWeight = undefined }]
+                        update (om@SampleMap{..}:maps)
+                          | cap == smapCap && sample_by == smapVerb && sample_type == smapNoun
+                          = let !samples' = IM.insert (fromIntegral t) ev smapSamples
+                            in om{smapSamples = samples'} : maps
+                          | otherwise
+                          = let !maps' = update maps in om:maps'
+                    in go (n+1) $! update maps
+      | otherwise = go (n+1) maps
+    setSampleWeights m =
+      let !weightTree = mkTimeTree (map weight $ IM.elems $ smapSamples m) endTime
+          weight (Event t (Samples {weights})) = (t, SampleWeight $ sum $ UA.elems weights)
+          weight _                             = error "weight: impossible"
+      in m { smapSampleWeight = weightTree  }
+
+-- | Gives a time tree for how much total sample weight we have for
+-- every given time period
+getSumTimeTree :: DebugMaps -> Int -> SampleVerb -> SampleNoun -> TimeTree SampleWeight
+getSumTimeTree DebugMaps{sampleMaps} cap verb noun
+  | Just smap <- lookupSampleMap sampleMaps cap verb noun
+  = smapSampleWeight smap
+  | otherwise
+  = mkTimeTree [] 0
+
+-- | Returns a time tree with the weights of all samples that fall
+-- into any of the ranges given
+getSampleTimeTree :: DebugMaps -> Int -> SampleVerb -> SampleNoun -> [DebugEntry] -> TimeTree SampleWeight
+getSampleTimeTree dmaps@DebugMaps{sampleMaps,endTime} cap verb noun entries
+  | Just smap <- lookupSampleMap sampleMaps cap verb noun
+  = let ranges = getExclusiveIPRanges dmaps entries
+        -- Bounds of ranges - small optimization. If this function
+        -- ever becomes a bottleneck, one optimization might be to
+        -- build a more fine-grained estimation for what memory
+        -- regions we are interested in.
+        !left = fromIntegral $ minimum $ 0 : map rangeStart ranges
+        !right = fromIntegral $ maximum $ 0 : map rangeEnd ranges
+        build (Event t (Samples {samples=samples, weights=weights})) evs
+          = let (low, high) = UA.bounds samples
+                go !i !acc
+                  | i > high   = acc
+                  | ip < left  = go (i+1) acc
+                  | ip > right = go (i+1) acc
+                  | any (fromIntegral ip `inIPRange`) ranges
+                               = go (i+1) (acc + weights UA.! i)
+                  | otherwise  = go (i+1) acc
+                  where ip = samples UA.! i
+            in case go low 0 of
+              0 | (_, SampleWeight 0):_ <- evs
+                -> (t, SampleWeight 0):tail evs
+              w -> (t, SampleWeight w):evs
+        build _other _ = error "getSampleTimeTree: impossible"
+                         -- buildSampleMaps should have made sure of that
+    in mkTimeTree (IM.foldr build [] (smapSamples smap)) endTime
+  | otherwise
+  = mkTimeTree [] 0
+
+-- | Gives IP ranges for given debug entries. Also sorts & optimizes the ranges.
+getExclusiveIPRanges :: DebugMaps -> [DebugEntry] -> [IPRange]
+getExclusiveIPRanges DebugMaps{rangeMap} entries =
+  let rawRanges = sortBy (comparing rangeStart) $ concatMap dbgRanges entries
+
+      -- Some IP ranges might overlap -- this is mainly an optimization.
+      merge (r1@(IPRange s1 e1) : rs1@((IPRange s2 e2) : rs2))
+        | s2 <= e1   = merge (IPRange s1 (e1 `max` e2) : rs2)
+        | otherwise  = r1 : merge rs1
+      merge rs       = rs
+      mergedRanges = merge rawRanges
+
+      -- IP ranges might actually theoretically belong to other, more
+      -- specific, DebugEntries. We check the rangeMap to make sure of
+      -- that.
+      extractRanges (IPRange start end) = go Nothing $ IM.assocs rmap
+        where rmap = fst $ IM.split (end+1) $
+                     snd $ IM.split (start-1) rangeMap
+              go Nothing  []         = []
+              go (Just x) []         = [IPRange x end]
+              go Nothing  ((t,e):rs)
+                | e `elem` entries   = go (Just t) rs
+                | otherwise          = go Nothing  rs
+              go (Just x) ((t,e):rs)
+                | e `elem` entries   = go (Just x) rs
+                | otherwise          = (IPRange x t) : go Nothing rs
+              -- NB: The loop gets started with "Nothing", implying
+              -- that not encountering a range map entry exactly at
+              -- "start" means that the start of the range isn't
+              -- covered. This is, in fact, true because the range map
+              -- was generated from debug entries, therefore the
+              -- unlikely case of our "start" not appearing means that
+              -- we actually were overwritten by a range that's not a
+              -- member of "entries".
+
+  in concatMap extractRanges mergedRanges
 
 -------------------------------------------------------------------------------
 -- Utils
